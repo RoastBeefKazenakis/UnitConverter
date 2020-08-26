8a36e848c1d6f56487d13af6738f8816b42d5857
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2020-08-26 13:14:18 -0400

    Pounds to Kilos function

---
+++ Sources/UnitConverter/UnitConverter.swift
@@ -0,0 +1,9 @@
+//Just beacuse the struct is public, doesn't mean everything inside it is too. You must mark everything you want to be externally accessible with an access control level of public
+public struct UnitConverter {
+    
+    private let secretNumber = 42
+   
+    public func poundsToKilos(_ pounds: Double) -> Double {
+        return pounds * 2.2
+    }
+}
